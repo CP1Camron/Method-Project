5037a758b467488a6536840cbb4b0d1cc5add9ba
Camron Caldwell <camron.caldwell.2022@student.lhschools.org>
2019-10-17 09:34:10 -0400

    Added "I really like..." method

---
+++ src/com/LickingHeights/Main.java
@@ -0,0 +1,27 @@
+package com.LickingHeights;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        printGreetings("World");
+        printGreetings("Stars");
+        printGreetings("Universe");
+
+        System.out.println("Hello World");
+        System.out.println("Hello Stars");
+        System.out.println("Hello Universe");
+        System.out.println("Hello Moon");
+
+    }
+
+
+
+    public static void printGreetings(String noun){
+        System.out.println("Hello" +noun);
+
+
+
+    }
+}
